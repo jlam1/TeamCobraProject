c78e1855358b547ff699ddd9360ada311f956a52
[King <kingklo92@gmail.com>
2016-10-15 14:02:27 -0400

    Changed the String roomNumber to int roomNumber.
    Added a comment on how the room will be numbered in the arrayList
    
    --> King Lo

--- UndergroundHero/src/Room.java
+++ UndergroundHero/src/Room.java
@@ -1,12 +1,31 @@
 public class Room {
 
-	private String roomNumber;
+	private int roomNumber;
 	private String roomDescription;
 	private String exits;
 	private Monster roomMonster;
 	private Puzzle roomPuzzle;
 	
-	Room(String roomNumber, String roomDescription, String exits, Monster roomMonster, Puzzle roomPuzzle){
+	/*
+	 * Room willl be in integer number instead of strings.
+	 * ArrayList: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |
+	 *            ---------------------------------------
+	 *   Floor 1: 11  12  13  14  15  16  17  18  19 
+	 * 
+	 * ArrayList: 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 |
+	 *            ---------------------------------------
+	 *   Floor 2: 21  22   23   24   25   26   27   28   29 
+	 *   
+	 * ArrayList: 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 |
+	 *            ---------------------------------------
+	 *   Floor 3: 31   32   33   34   35   36   37   38   39  310
+	 * 
+	 * ArrayList: 28 | 29 | 30 | 31 | 32 | 33 | 34 | 35 | 36 | 37 | 38  |  39  | 40  |  41
+	 *            ---------------------------------------  	  
+	 *   Floor 4: 41   42   43   44   45   46   47   48   49  410   411   412    413    414
+	 */       
+	
+	Room(int roomNumber, String roomDescription, String exits, Monster roomMonster, Puzzle roomPuzzle){
 		this.roomNumber = roomNumber;
 		this.roomDescription = roomDescription;
 		this.exits = exits;
@@ -14,11 +33,11 @@ public class Room {
 		this.roomPuzzle = roomPuzzle;
 	}
 
-	public String getRoomNumber() {
+	public int getRoomNumber() {
 		return roomNumber;
 	}
 
-	public void setRoomNumber(String roomNumber) {
+	public void setRoomNumber(int roomNumber) {
 		this.roomNumber = roomNumber;
 	}
 
