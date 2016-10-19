b12043a7061fade8d4338fb7795e162e98593e82
[King <kingklo92@gmail.com>
2016-10-19 12:44:06 -0400

    Added a Inventory class has no method implemented yet -King

---
+++ UndergroundHero/src/Inventory.java
@@ -0,0 +1,19 @@
+
+
+/**Class name: Inventory.java
+ * @author King Lo
+ * @version 1.0
+ * Course: ITEC 3150 Fall 2016
+ * Written: Oct 19, 2016
+ *
+ * 
+ * This class –now describe what the class does
+ * 
+ * Purpose: –Describe the purpose of this class 
+ */
+public class Inventory extends Item
+{
+
+	
+	
+}
