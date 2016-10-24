644fedc018716c0eb707dca643153eceb92ee146
[King <kingklo92@gmail.com>
2016-10-24 13:27:19 -0400

    Made major changes. delete and added the save and load methods from Game to Resource manager Added a saveLoadData interface to initiate the save and load. made a working save and load data

---
+++ UndergroundHero/src/saveLoadData.java
@@ -0,0 +1,16 @@
+
+public class saveLoadData implements java.io.Serializable
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	public String roomNumber;
+	public int hp;
+	public int attack;
+	public int speed;
+	public int def;
+	public String roomDescription;
+}
