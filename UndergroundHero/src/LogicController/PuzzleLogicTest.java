7ec4f20744e2daaf34d467acdde8bd91a5b8e90b
William Smith <wsmith@ggc.edu>
2016-11-20 23:10:40 -0500

    added junit4 to classpath to fix junit errors

---
+++ UndergroundHero/src/LogicController/PuzzleLogicTest.java
@@ -0,0 +1,19 @@
+package LogicController;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PuzzleLogicTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
